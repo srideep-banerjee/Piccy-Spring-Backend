19b6115e8cf8a63b0c75a29b4b8a890ff7b58a8d
srideep-banerjee <banerjee.srideep@gmail.com>
2025-04-28 16:00:42 +0530

    Improved error handling in PostControllerAdvice

--- src/main/java/me/projects/piccy/posts/PostControllerAdvice.java
+++ src/main/java/me/projects/piccy/posts/PostControllerAdvice.java
@@ -4,17 +4,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 class PostControllerAdvice {
 
     @ExceptionHandler(value = {PostNotFoundException.class})
-    ResponseEntity<String> handlePostNotFound(PostNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    ResponseEntity<Map<String, String>> handlePostNotFound(PostNotFoundException e) {
+        return new ResponseEntity<>(getErrorPayload(e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = {PostCreatorMismatchException.class})
-    ResponseEntity<String> handleCreatorMismatch() {
-        return new ResponseEntity<>("Action exclusively available to creator", HttpStatus.FORBIDDEN);
+    ResponseEntity<Map<String, String>> handleCreatorMismatch() {
+        return new ResponseEntity<>(
+                getErrorPayload("Action exclusively available to creator"),
+                HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler(value = {ResponseStatusException.class})
+    ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException e) {
+        return new ResponseEntity<>(getErrorPayload(e.getReason()), e.getStatusCode());
+    }
+
+    Map<String, String> getErrorPayload(String body) {
+        Map<String, String> response = new HashMap<>();
+        response.put("cause", body);
+        return response;
     }
 }
