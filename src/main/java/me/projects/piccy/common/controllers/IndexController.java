f95fae3dc450443d5515fce64485d06ad91d977f
srideep-banerjee <banerjee.srideep@gmail.com>
2025-06-09 22:43:40 +0530

    Fixed index assets related bug

--- src/main/java/me/projects/piccy/common/controllers/IndexController.java
+++ src/main/java/me/projects/piccy/common/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package me.projects.piccy.common.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping
-    private String serveIndex() {
-        return "index.html";
+    @GetMapping(value = "/{path1:.*}/{path2:^[^.]*$}")
+    private String serveIndex(HttpServletRequest request) {
+        return "/index.html";
     }
 }
