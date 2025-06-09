dba22f546facc43a318ced7e73f74f9e7b6fbd24
srideep-banerjee <banerjee.srideep@gmail.com>
2025-06-09 12:29:14 +0530

    Fixed bug in signup endpoint

--- src/main/java/me/projects/piccy/auth/AuthController.java
+++ src/main/java/me/projects/piccy/auth/AuthController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/api")
+@Controller
+@RequestMapping(path = "/api")
 public class AuthController {
 
     private final AuthUserService authUserService;
