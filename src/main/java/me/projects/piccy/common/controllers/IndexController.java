fa764bd147d8eb32dc20a4d3a5fb11221cc9503f
srideep-banerjee <banerjee.srideep@gmail.com>
2025-06-11 11:12:44 +0530

    Fixed bug in IndexedController

--- src/main/java/me/projects/piccy/common/controllers/IndexController.java
+++ src/main/java/me/projects/piccy/common/controllers/IndexController.java
@@ -1,14 +1,13 @@
 package me.projects.piccy.common.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping(value = "/{path1:.*}/{path2:^[^.]*$}")
-    private String serveIndex(HttpServletRequest request) {
+    @GetMapping(value = {"/{path:^[^.]*$}", "/*/{path:^[^.]*$}", "/*/*/{path:^[^.]*$}"})
+    private String serveIndex() {
         return "/index.html";
     }
 }
